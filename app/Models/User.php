8d11ad47e8c3b5c823004f4b75e26760b4635da6
Blaise <menniablaise@yahoo.com>
2026-03-30 17:46:10 +0000

    feat: add attendance relationship to User model and calculate teacher attendance statistics for the dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(TeacherProfile::class);
     }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
 }
